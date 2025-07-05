41fa431b0d0c63825a9e38457d8473258d30b046
sty <scarsty@gmail.com>
2025-07-05 23:44:36 +0800

    修改编码为utf8

--- cp.bat
+++ cp.bat
@@ -1,4 +1,5 @@
 copy D:\kys-all\_cpp\kys-cpp\x64\Release\kys.exe bin\
+copy D:\kys-all\相关开发\AllMod-Launcher\Launcher\bin\Release\AllMod-Launcher.exe cpplauncher.exe
 copy ..\x64\release\kys.exe .
 call spreadtitle.bat game-dragon
 call spreadtitle.bat game-ptt
