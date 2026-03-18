0c799a80e0308d75ae9b75fc9f6bc82a9cd0988d
scarsty <scarsty@gmail.com>
2026-03-18 16:14:19 +0800

    删除awaken

--- make7z.bat
+++ make7z.bat
@@ -1,4 +1,4 @@
 call cp.bat
-del kys-cpp-5in1.7z
+del kys-cpp-4in1.7z
 copy ..\x64\release\kys.exe bin\
-7za a -mx9 -t7z kys-cpp-5in1.7z ./bin ./game ./game-dragon ./game-ptt ./game-awaken ./game-RiverLake play.bat cpplauncher.exe games.ini
+7za a -mx9 -t7z kys-cpp-4in1.7z ./bin ./game ./game-dragon ./game-ptt ./game-RiverLake play.bat cpplauncher.exe games.ini
