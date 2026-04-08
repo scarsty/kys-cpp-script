31f592110a067440bfec457a45b771182339657f
scarsty <scarsty@gmail.com>
2026-04-09 00:40:03 +0800

    有些改动

---
+++ make7z2x.bat
@@ -0,0 +1,4 @@
+call cp.bat
+rem del kys-cpp-2x.zip
+copy ..\x64\release\kys.exe bin\
+7za a kys-cpp-2x.zip ./bin ./game2x play2x.bat
---
+++ play2x.bat
@@ -0,0 +1,3 @@
+#copy ..\x64\release\kys.exe bin\
+#copy ..\x64\release\battle.dll bin\
+start .\bin\kys.exe .\game2x
