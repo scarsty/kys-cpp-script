84d7a068a24031f62f432227f8b8420280955234
scarsty <scarsty@gmail.com>
2026-04-28 00:34:56 +0800

    pig3 game0脚本错误

--- pig3/cp.bat
+++ pig3/cp.bat
@@ -1,3 +1,4 @@
 copy ..\..\..\pig3\kys-pig3\game\script\*.lua game\script\
-copy ..\..\..\pig3\kys-pig3\game\script\event\*.lua game\script\event\
+copy ..\..\..\_pascal\pig3\kys-pig3\game\script\event\*.lua game\script\event\
+copy ..\..\..\_pascal\pig3\kys-pig3\game0\script\event\*.lua game0\script\event\
 pause
